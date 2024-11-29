b3002e986c945b74ba1bbf341f81c1e1d7c953cc
Zhongjie LI <lzj44010@gmail.com>
2024-11-29 21:01:46 +0100

    réorganisation de fichiers

--- README.md
+++
@@ -1,5 +0,0 @@
-Commande à exécuter dans le terminal : 
-
-sh **./analyseur_executable.sh annule_arg.TS**
-
-Les résultats sont affichés dans le terminal et sauvegardés dans **result.txt**.
--- analyseur_executable.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-input_file_path="$1"
-
-if [ $# -ne 1 ]; then
-    echo "Il faut un argument qui est le nom complet du fichier texte d'entrée."
-else
-    python analyseur.py "$input_file_path" | tee result.txt
-fi
